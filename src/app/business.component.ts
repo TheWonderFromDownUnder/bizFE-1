31fcccda2a57433ff40a06081e9487fd9a8ad4e4
TheWonderFromDownUnder <52037295+TheWonderFromDownUnder@users.noreply.github.com>
2024-12-08 18:26:10 +0000

    New change: Continuing from v17 setup to v18.

---
+++ src/app/business.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, ActivatedRoute } from '@angular/router';
+import { DataService } from './data.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'business',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  providers: [DataService],
+  templateUrl: './business.component.html',
+  styleUrl: './business.component.css'
+})
+export class BusinessComponent {
+  business_list: any;
+
+  constructor( public dataService: DataService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+      this.business_list = this.dataService.getBusiness(this.route.snapshot.paramMap.get('id'));
+  }
+}
