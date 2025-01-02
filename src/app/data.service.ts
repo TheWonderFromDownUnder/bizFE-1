d4f699ff6e05767e2234cc407aae1023658ad13e
TheWonderFromDownUnder <52037295+TheWonderFromDownUnder@users.noreply.github.com>
2025-01-02 07:33:52 +0000

    New changes: Added star icon to business reviews, added google map function to businesses.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,16 @@
 import jsonData from "../assets/businesses.json";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+@Injectable( {
+  providedIn: "root",
+})
 export class DataService {
 
   pageSize: number = 4;
+
+  constructor(private http: HttpClient) {}
 
   getBusiness(id: any) {
     let dataToReturn: any[] = [];
@@ -22,5 +30,15 @@ export class DataService {
 
   getLastPageNumber() {
     return Math.ceil( jsonData.length / this.pageSize);
+  }
+
+  getLoremIpsum(paragraphs: number): Observable<any> {
+
+    let API_key = 'kTn9xnr9ooo7hJeQtben/A==S9t4CTzOBc3c8uQ2';
+    return this.http.get<any>(
+                     'https://api.api-ninjas.com/v1/' +
+                     'loremipsum?paragraphs=' + paragraphs,
+               { headers: { 'X-Api-Key': API_key } }
+    );
   }
 }
