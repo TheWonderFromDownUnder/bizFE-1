4359c154f2c9527cfc095525a5a3ab526eb50f4d
TheWonderFromDownUnder <52037295+TheWonderFromDownUnder@users.noreply.github.com>
2025-01-06 02:56:02 +0000

    New change: Connected the old business API to the new web page.

--- src/app/businesses.component.ts
+++ src/app/businesses.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { DataService } from './data.service';
+import { WebService } from './web.service';
 
 @Component({
   selector: 'businesses',
   standalone: true,
   imports: [RouterOutlet, RouterModule],
-  providers: [DataService],
+  providers: [DataService, WebService],
   templateUrl: './businesses.component.html',
   styleUrl: './businesses.component.css'
 })
@@ -15,20 +16,28 @@ export class BusinessesComponent {
   business_list: any;
   page: number = 1;
 
-  constructor(public dataService: DataService) { }
+  constructor(public dataService: DataService, private webService: WebService) { }
 
   ngOnInit() {
     if (sessionStorage["page"]) {
       this.page = Number(sessionStorage["page"]);
     }
-    this.business_list = this.dataService.getBusinesses(this.page);
+   // this.business_list = this.dataService.getBusinesses(this.page);
+
+   this.webService.getBusinesses(this.page)
+    .subscribe((response) => {
+      this.business_list = response;
+    })
   }
 
   previousPage() {
     if (this.page > 1) {
     this.page = this.page - 1
     sessionStorage["page"] = this.page;
-    this.business_list = this.dataService.getBusinesses(this.page);
+    this.webService.getBusinesses(this.page)
+    .subscribe((response: any) => {
+       this.business_list = response;
+    })
     }
   }
 
@@ -36,7 +45,10 @@ export class BusinessesComponent {
     if (this.page < this.dataService.getLastPageNumber())
     this.page = this.page + 1
     sessionStorage["page"] = this.page;
-    this.business_list = this.dataService.getBusinesses(this.page);
+    this.webService.getBusinesses(this.page)
+    .subscribe((response: any) => {
+       this.business_list = response;
+    })
   }
 
 /*  business_list =  [
