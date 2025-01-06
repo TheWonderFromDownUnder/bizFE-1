5c7091ca80f5247c6928522fb1e1ded95dfee677
TheWonderFromDownUnder <52037295+TheWonderFromDownUnder@users.noreply.github.com>
2025-01-06 02:15:12 +0000

    New change: Used Auth0 to integrate login functionality into application.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthButtonComponent } from "./authbutton.component";
+import { AuthUserComponent } from "./authuser.component";
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthButtonComponent, AuthUserComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAuth0({
+      domain: 'dev-61pveroy57m608n5.us.auth0.com',
+      clientId: 'MvTsy2vsZjCA8T0oHZ5PMSlpcCD4jzf4',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+    provideHttpClient(),
+    appConfig.providers
+  ]
+}).catch((err) => console.error(err));
